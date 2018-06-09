60b1c4902f206bf4e8b31267ec3b1c5e2befbdbe
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-09 14:14:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,3 +27,9 @@ def remove_element_from_start_of_array(element)
   array = ["wow", "1", "2", "3", "4"]
   array.shift["wow"]
 end
+
+def retrieve_element_from_index(array, 2)
+  array = ["wow", "1", "2", "3", "4", "arrays"]
+  index_number = 
+  
+end
