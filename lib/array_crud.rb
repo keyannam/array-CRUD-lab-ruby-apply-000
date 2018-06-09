835d77abfa45f332dd4a63d9f4c09c4a26de4aa0
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-09 13:49:00 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,3 +11,8 @@ def add_element_to_end_of_array(element, array)
   element = "arrays!"
   array.push("arrays!")
 end
+
+def add_element_to_start_of_array(array, element)
+  array = ["1", "2", "3", "4"]
+  element = "wow"
+  array.unshift("wow")
