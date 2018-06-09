80842016620bdb5da9cf9f13063a88dfbc35652c
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-09 13:52:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,6 +19,6 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["wow","1", "2", "3", "4", "arrays!"]
+  array = [1", "2", "3", "4"]
   array.pop("arrays!")
 end
