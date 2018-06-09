95df94d22ced2e74b9fab6cc8cfafd023567fd24
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-09 13:44:30 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,6 +6,8 @@ def create_an_array
   ["1", "2", "3", "4"]
 end
 
-def add_element_to_end_of_array
-  ["1", "2", "3", "4"].push("5")
+def add_element_to_end_of_array(element, array)
+  array = ["1", "2", "3", "4"]
+  element = "5"
+  array.push("5")
 end
